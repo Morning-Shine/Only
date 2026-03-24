4ba98c3e565d75378fa48939f3f10bb35f767f14
Morning-Shine <maskacat@gmail.com>
2026-03-24 22:56:31 +0300

    задеплоен проект - Only

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+    publicPath: '/Only/',
     clean: true,
   },
   resolve: {
